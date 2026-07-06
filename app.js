28fca9d1410cf3e6be996ceab3453582fc8311b7
mnasikhunamin-jpg <mnasikhunamin@gmail.com>
2026-07-06 14:47:40 +0700

    Implement user login functionality in app.js
    Added loginSiswa function to handle user login and store data in localStorage.

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("MPI Smart Farming loaded");
+console.log("MPI aktif");
+
+function loginSiswa(){
+
+    let nama =
+        document.getElementById("nama").value;
+
+    let sekolah =
+        document.getElementById("sekolah").value;
+
+    if(nama=="" || sekolah==""){
+        alert("Silakan lengkapi data.");
+        return;
+    }
+
+    localStorage.setItem(
+        "nama", nama
+    );
+
+    localStorage.setItem(
+        "sekolah", sekolah
+    );
+
+    alert(
+        "Selamat datang " + nama
+    );
+
+    window.location.href =
+        "dashboard.html";
+}
